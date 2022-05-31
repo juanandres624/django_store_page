5675c4328b3730f75cd873334107af0ac672fcfe
juanandres624 <juanandres624@hotmail.com>
2022-05-31 15:15:08 -0500

    static change 3

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -145,12 +145,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #MEDIA_ROOT = BASE_DIR / 'media_root'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static_in_env'),
 ]
 
 # Default primary key field type
